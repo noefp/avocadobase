d1b3d5fed0ce7ef5200df9128dd4505bd1cfa0ba
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-03-14 13:26:55 +0100

    first expression dataset added

---
+++ egdb_custom_text/expr_datasets/01_hass_mesocarp.php
@@ -0,0 +1,59 @@
+<p>
+
+  RNA-seq of <i>Persea americana</i> pulp (mesocarp) fruit ripening from cv. Hass 
+
+</p>
+
+
+<h4>Experimental Conditions (3 replicates):</h4>
+
+
+  <ul>
+
+
+    <li>Hass pulp 150 DAFS</li>
+
+
+    <li>Hass pulp 240 DAFS</li>
+
+
+    <li>Hass pulp 300 DAFS</li>
+
+
+    <li>Hass pulp 390 DAFS</li>
+
+  </ul>
+
+
+<p>
+
+
+
+  Avocado fruits were obtained from a commercial supplier,
+  
+  three independent fruits (biological replicates) were sampled at 150, 240, 300 and 390 days after fruit set (DAFS).
+  
+  Three biological replicates for each developmental stage.
+  
+  cDNA libraries were sequenced by PE sequencing (100 x 2) with Illumina HiSeq 2500 and normalized to TPM.
+
+
+  
+
+
+  This dataset was published by <a href="https://doi.org/10.1186/s12864-019-5486-7" target="_blank">Vergara-Pulgar et al. 2019 </a> 
+
+
+  and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA483022" target="_blank">PRJNA483022</a>.
+
+
+</p>
+
+
+<br>
+
+
+
+<b>DAFS</b> = Days After Fruit Set
+
+
