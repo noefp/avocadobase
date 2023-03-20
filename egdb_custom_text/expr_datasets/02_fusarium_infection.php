70fb0ac3345c0d1cd58930cf3f1de40ec15e85a1
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-03-20 12:23:27 +0100

    new dataset description and images

---
+++ egdb_custom_text/expr_datasets/02_fusarium_infection.php
@@ -0,0 +1,23 @@
+<p>
+  RNA-seq of <i>Persea americana</i> stem inoculated with <i>Fusarium kuroshium</i> at time 1, 4, 7 and 14 days post inoculation.
+</p>
+
+<h4>Experimental Conditions (3 replicates per sample):</h4>
+
+<ul>
+    <li>Hass stem 1, 4, 7 and 14 under control conditions.</li>
+    <li>Hass stem 1, 4, 7 and 14 days post inoculation.</li>
+</ul>
+
+<p style="text-align:justify">
+  One-year-old healthy avocado trees (<i>P. americana</i> 'Hass') were acquired from the Salas certified plant nursery at Uruapan, Michoacán, Mexico.
+
+  Infected and uninfected stems (at 1, 4, 7 and 14 dpi) were frozen with liquid nitrogen and then pulverized using a mortar and pestle.
+  
+  Three independent biological replicates from each selected sample were processed. cDNA libraries were sequenced by PE sequencing (100 x 2) with NextSeq500 and normalized to TPM.
+This dataset was published by <a href="https://doi.org/10.7717/peerj.11215" target="_blank">Pérez-Torres et al. 2021 </a> and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA655460" target="_blank">PRJNA655460</a>.
+</p>
+
+<b>DPI</b> = Days Post Inoculation
+<br>
+<img class='rounded' src='<?php echo "$images_path/expr/fusarium_images.jpg";?>' width="800px" alt='avocado Fusarium infection'>
