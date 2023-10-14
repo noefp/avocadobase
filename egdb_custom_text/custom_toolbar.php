ad8f103f9e96d368a79b81c00e65d2a8f0712bf6
Enrique López Gómez <enrique.elg.93@gmail.com>
2023-10-14 19:36:18 +0200

    Added Links & Documents in the More tab in the toolbar

--- egdb_custom_text/custom_toolbar.php
+++ egdb_custom_text/custom_toolbar.php
@@ -7,3 +7,9 @@
   </div>
 </li>
 <li class="nav-item"><a class="nav-link" href="/easy_gdb/custom_view.php?file_name=genome.php">Genomes</a></li>
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">More</a>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=links.php">Links & Documents</a>
+  </div>
+</li>
