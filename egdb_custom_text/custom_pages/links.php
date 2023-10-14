5131b70256ce78def66706858a53fb73ddb6a7e7
Enrique López Gómez <enrique.elg.93@gmail.com>
2023-10-14 19:55:56 +0200

    New Links & Documents page

---
+++ egdb_custom_text/custom_pages/links.php
@@ -0,0 +1,12 @@
+<div class="width900">
+  <h1 style="font-size:26px">Links of interest</h1>
+  <br>
+  <div class="list-group">
+    <a href="https://www.avocadosource.com" class="list-group-item list-group-item-action" target="_blank">Avocado Source</a>
+    <a href="/downloads/PDFs/Descriptors_avocado_IPGRI1995.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Descriptors for avocado</a>
+    <a href="/downloads/PDFs/Guia_GIP_aguacate.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Guía de gestión integrada de plagas del aguacate</a>
+    <a href="/downloads/PDFs/Manual_calidad_aguacate.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Manual de calidad del aguacate</a>
+    <a href="/downloads/PDFs/Manual_manejo_practico_cultivo_aguacate.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Manual de manejo práctico del cultivo de aguacate</a>
+    <a href="/downloads/PDFs/Valor_nutricional_aguacate.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Valor nutricional aguacate Fen.org.es</a>
+  </div>
+</div>
